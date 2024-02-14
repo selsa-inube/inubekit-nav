38733fa7ab1a7598b34b8555b513248bc52f6465
Andres Babativa <carlos3k11@gmail.com>
2024-02-14 09:43:57 -0500

    feat: implement the Nav component and its dependencies

---
+++ src/Nav/stories/Nav.WithoutSections.stories.tsx
@@ -0,0 +1,88 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  MdKey,
+  MdMeetingRoom,
+  MdPhone,
+  MdBadge,
+  MdStarBorder,
+  MdAccountBalance,
+  MdAccountBalanceWallet,
+} from "react-icons/md";
+
+import { Nav, INavProps } from "..";
+
+import { props } from "../props";
+
+const story = {
+  title: "navigation/Nav",
+  components: Nav,
+  argTypes: props,
+  decorators: [
+    (Story: React.ElementType) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export const WithoutSections = (args: INavProps) => <Nav {...args} />;
+
+WithoutSections.args = {
+  navigation: {
+    title: "MENU",
+    sections: {
+      administrate: {
+        name: "Administrate",
+        links: {
+          privileges: {
+            id: "privileges",
+            label: "Privileges",
+            icon: <MdKey />,
+            path: "/privileges",
+          },
+          accounting: {
+            id: "accounting",
+            label: "Accounting",
+            icon: <MdMeetingRoom />,
+            path: "/accounting",
+          },
+          contacts: {
+            id: "contacts",
+            label: "Contacts",
+            icon: <MdPhone />,
+            path: "/contacts",
+          },
+          documents: {
+            id: "documents",
+            label: "Documents",
+            icon: <MdBadge />,
+            path: "/documents",
+          },
+          marketing: {
+            id: "marketing",
+            label: "Marketing",
+            icon: <MdStarBorder />,
+            path: "/marketing",
+          },
+          savings: {
+            id: "savings",
+            label: "Savings",
+            icon: <MdAccountBalanceWallet />,
+            path: "/savings",
+          },
+          credit: {
+            id: "credit",
+            label: "Credit",
+            icon: <MdAccountBalance />,
+            path: "/credit",
+          },
+        },
+      },
+    },
+  },
+  logoutPath: "/logout",
+  logoutTitle: "logout",
+};
+
+export default story;
---
+++ src/Nav/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { inube } from "@inubekit/foundations";
+
+export const StyledNav = styled.div`
+  width: 248px;
+  box-sizing: border-box;
+  background-color: ${({ theme }) =>
+    theme?.color?.surface?.nav?.regular || inube.color.surface.nav.regular};
+  border-right: 1px solid
+    ${({ theme }) =>
+      theme?.color?.stroke?.divider?.regular ||
+      inube.color.stroke.divider.regular};
+`;
+
+export const StyledFooter = styled.footer`
+  width: 100%;
+`;
+
+export const SeparatorLine = styled.div`
+  width: calc(100% - 32px);
+  margin: ${({ theme }) =>
+    `${theme?.spacing?.s100 || inube.spacing.s100} ${
+      theme?.spacing?.s200 || inube.spacing.s200
+    }`};
+  height: 1px;
+  padding: ${({ theme }) => theme?.spacing?.s0 || inube.spacing.s0};
+  background-color: ${({ theme }) =>
+    theme?.color?.stroke?.divider?.regular ||
+    inube.color.stroke.divider.regular};
+`;
