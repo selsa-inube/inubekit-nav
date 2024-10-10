30dfd1af856c6e2fe0fb1fea2be210f609da3e11
Cesar Marin Alfonso <45011420+cmarin001@users.noreply.github.com>
2024-10-10 11:24:13 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { NavLink } from "./NavLink";
 export type { INav, INavAction } from "./Nav";
 export type { INavLink } from "./NavLink";
 export type { ILink, INavSection, INavNavigation } from "./Nav/props";
+export { tokens as NavTokens } from "./Nav/Tokens/tokens";
